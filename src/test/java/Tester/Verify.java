5872024f5623824ef92577198eeb8b10b86fa19a
gowthamprostack <mail2gowthamreddy@gmail.com>
2025-01-31 08:20:37 +0530

    branchtwo

--- src/test/java/Tester/Verify.java
+++ src/test/java/Tester/Verify.java
@@ -4,7 +4,22 @@ public class Verify {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("branchtwo");
+		System.out.println("branchtwo");
+		System.out.println("branchtwo");
+		System.out.println("branchtwo");
+		System.out.println("branchtwo");
 
 	}
-
+	
+	public void branchtwo()
+	{
+		System.out.println("branchtwo");
+	}
+	
+	public void run()
+	{
+		System.out.println("branchtwo");
+	}
+	
 }
