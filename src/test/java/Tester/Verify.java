97933325f6550255605e31f6e9be6a7d2af7b4ef
gowthamprostack <mail2gowthamreddy@gmail.com>
2025-01-31 08:07:06 +0530

    pusining to branch one

--- src/test/java/Tester/Verify.java
+++ src/test/java/Tester/Verify.java
@@ -4,7 +4,17 @@ public class Verify {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("workingbranchone");
+		System.out.println("workingbranchone");
+		System.out.println("workingbranchone");
+		System.out.println("workingbranchone");
+		
 
 	}
-
+	
+	
+	public void run()
+	{
+		System.out.println("Public void runbranch one");
+	}
 }
